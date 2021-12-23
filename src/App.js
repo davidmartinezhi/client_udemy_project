fc32e651ae5b7a49f7f0a3e811b32ee9ba24be0e
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-22 23:18:47 -0600

    Agregado alias a BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 import "./App.scss";
 
