bb7123dc2ebc738f06be7f3079fa898014db8f2b
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-23 21:00:07 -0600

    Vaciada la app.js

--- src/App.js
+++ src/App.js
@@ -1,64 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 import "./App.scss";
 
 function App() {
   
   return (
-    <Router>
-      <div className="App">
-      
-        <h1>Sistema de rutas basico</h1>
-        <Link to="/">Home</Link> 
-        <br/>
-        <Link to="/contact">Contact</Link> 
-        <br/>
-        <Link to="/user">User</Link> 
-        <br/> <br/> <br/> <br/>
-
-        <Routes>
-          <Route exact path="/"  element={<Home/>} />
-          <Route exact path="/contact" element={<Contact/>} />
-          <Route exact path="/user" element={<User/>} />
-          <Route path="*" element={<Error404/>} />
-        </Routes>
-      </div>
-    </Router>
+    <h1>Estamos en App.js</h1>
   );
 }
 
-//Creo los componentes
-function Home () {
-  return (
-    <div>
-      <h2>Estamos en el componente Home</h2>
-    </div>
-  );
-}
-
-function Contact () {
-  return (
-    <div>
-      <h2>Componente Contact...</h2>
-    </div>
-  );
-}
-
-function User () {
-  return (
-    <div>
-      <h2>Estamos en el componente User</h2>
-    </div>
-  );
-}
-
-function Error404 () {
-  return (
-    <div>
-      <h2>Error 404...</h2>
-    </div>
-  );
-}
 
 export default App;
