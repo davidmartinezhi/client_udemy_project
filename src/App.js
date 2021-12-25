5547f0d2fa64deb50ce135a865b6bfc5177d0422
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-24 19:33:52 -0600

    Rendrizada la ruta de ejemplo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/admin" exact="true" element={<AdminHome/>}/>
+        <Route path="/admin" exact={true} element={<AdminHome/>}/>
       </Routes>
     </Router>
   );
