9a0e4fb4f01792fefdb703a699a703449e1e38dd
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-24 18:37:57 -0600

    Vaciado el return de app y removidas todas las importaciones

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import Admin from './pages/Admin';
-import SignIn from './pages/Admin/SignIn';
-import Home from './pages/Home'
-import Contact from './pages/Contact';
 
 import "./App.scss";
 
 function App() {
-  
-  return (
-    <div>
-      <h1>Estamos en App.js</h1>
-      <Admin />
-      <SignIn />
-      <Home />
-      <Contact />
-    </div>
-  );
+  return ();
 }
 
 
