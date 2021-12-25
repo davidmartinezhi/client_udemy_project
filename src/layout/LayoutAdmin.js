58fe954d119d79fc8a2df19eb1f8044326d4c78a
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-24 18:13:25 -0600

    Creado layot basico de administrador

--- src/layout/LayoutAdmin.js
+++ src/layout/LayoutAdmin.js
@@ -3,8 +3,12 @@ import { Layout } from 'antd';
 
 export default function LayoutAdmin() {
     return(
-        <div>
-
-        </div>
+        <Layout>
+            <h2>Menú Sidebar</h2>
+            <div>
+                Contenido....
+            </div>
+            <h5>Footer...</h5>
+        </Layout>
     );
 }
