fc59d972c5d994fcc83cd407f1b8f75580b73e14
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-24 18:40:14 -0600

    Importado el sistema de rutas y librerias

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import routes from './config/routes'
 import "./App.scss";
 
 function App() {
