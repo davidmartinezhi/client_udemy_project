40f96ad6c4af6e0c9af52984ced9eb306bd4b20c
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-28 21:45:01 -0600

    Agregados comentarios en funcion RouteWithSubRoutes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import routes from './config/routes';
-import AdminHome from './pages/Admin';
+
 import "./App.scss";
 
 function App() {
