d345ce8ffe1ca49afa8c1a794c8badd2a5a6bbf4
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-28 22:12:38 -0600

    Sistema de rutas a pages terminado y funcional

--- src/layout/LayoutBasic.js
+++ src/layout/LayoutBasic.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import {Layout} from 'antd';
 
-export default function LayoutBasic(){
+import './LayoutBasic.scss';
+
+export default function LayoutBasic( props ){
+    const { routes } = props;   //Sistema de rutas que me llega por props
+    const { Content , Footer } = Layout; //Para usar de manera directa esos componentes
+
     return(
         <Layout>
-            <h2>Menu sidebar</h2>
-            <div>Contenido...</div>
-            <h5>Footer..</h5>
+            <h2>Menu...</h2>
+            <Layout>
+                <Content>
+                    <LoadRouters routes={routes} />
+                </Content>
+                <Footer>
+                    David Gerardo Martínez
+                </Footer>
+            </Layout>
         </Layout>
+    );
+}
+
+
+function LoadRouters( {routes} ){
+    return(
+        routes.map((route, index) => (
+            <Route
+                key = {index}
+                path = {route.path}
+                exact = {route.exact}
+                component = {route.component}
+            />
+        ))
     );
 }
