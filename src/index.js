641b2facc6b55ce46062c4dc12c49888322dfc17
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-28 21:45:43 -0600

    Quitada la libreria react-router-dom

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "antd/dist/antd.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   
