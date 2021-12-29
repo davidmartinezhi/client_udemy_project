89685d2f2d9799008b5052404e7d1b3ed9a9fbb0
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-28 20:24:03 -0600

    React-Router-Dom @ 5.0.0 instalado

--- src/layout/LayoutAdmin.js
+++ src/layout/LayoutAdmin.js
@@ -1,14 +1,46 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Layout } from 'antd';
 
-export default function LayoutAdmin() {
+import "./LayoutAdmin.scss";
+import routes from "../config/routes";
+
+export default function LayoutAdmin( props ) {
+    const { routes } = props;
+    const { Header, Content, Footer } = Layout;
+
     return(
         <Layout>
             <h2>Menú Sidebar</h2>
-            <div>
-                Contenido....
-            </div>
-            <h5>Footer...</h5>
+            <Layout>
+                <Header>Header...</Header>
+
+                <Content>
+                
+                    <LoadRouters routes={routes}/>
+    
+                </Content>
+
+                <Footer>
+                    David Gerardo Martínez
+                </Footer>
+            </Layout>
+            
         </Layout>
     );
+}
+
+
+function LoadRouters ({routes}){
+    console.log(routes);
+    return routes.map((route, index) => (
+        <Routes>
+            <Route 
+                key={index}
+                path = {route.path}
+                exact = {route.exact}
+                element = {<route.component />}
+            />
+        </Routes>
+    ));
 }
