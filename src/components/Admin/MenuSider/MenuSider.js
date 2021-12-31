9e76b5b2df0bee8a8c0a9ab596037d510b409876
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2021-12-30 19:31:39 -0600

    Initial setup para el menu sider

---
+++ src/components/Admin/MenuSider/MenuSider.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import {Layout, Menu } from 'antd';
+//import {} from '@ant-design/icons';
+
+import './MenuSider.scss';
+
+export default function MenuSider () {
+    return(
+        "Menu Sider ..."
+    );
+}
