017aa063a4cd59aa407b6a98215dae7ee1f77ab6
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-03 20:58:05 -0600

    Agregado signin

--- src/layout/LayoutAdmin.js
+++ src/layout/LayoutAdmin.js
@@ -1,43 +1,56 @@
 import React, { useState } from "react";
-import { Route , Switch } from 'react-router-dom';
-import { Layout } from 'antd';
+import { Route , Switch , Redirect } from "react-router-dom";
+import { Layout } from "antd";
+//import useAuth from "../hooks/useAuth";
 import MenuTop from "../components/Admin/MenuTop";
 import MenuSider from "../components/Admin/MenuSider";
+import AdminSignIn from "../pages/Admin/SignIn/SignIn";
 
 import "./LayoutAdmin.scss"; 
 
-export default function LayoutAdmin( props ) {
-    
-    const { routes } = props;
-    const { Header, Content, Footer } = Layout;
-    
-    const [ menuCollapsed, setMenuCollapsed ] = useState(false);
+export default function LayoutAdmin(props) {
+  const { routes } = props;
+  const { Header, Content, Footer } = Layout;
+  //const { user, isLoading } = useAuth();
+  const [menuCollapsed, setMenuCollapsed] = useState(false);
 
-    return(
-        <Layout>
-            {/* TO DO: Menu Sidebar */}
-            <MenuSider menuCollapsed={menuCollapsed}/>
-            <Layout 
-            className="layout-admin"
-            style={{marginLeft: menuCollapsed ? "80px" : "200px"}}
-            >
-                <Header className="layout-admin__header">
-                    <MenuTop menuCollapsed={menuCollapsed} setMenuCollapsed={setMenuCollapsed}/>
-                </Header>
-
-                <Content className="layout-admin__content" >
-                
-                    <LoadRoutes routes = {routes}/>
-    
-                </Content>
-
-                <Footer className="layout-admin__footer">
-                    David Gerardo Martínez
-                </Footer>
-            </Layout>
-            
-        </Layout>
+  const user = null;
+  
+  if (!user) {
+    return (
+      <>
+        <Route path="/admin/login" component={AdminSignIn} />
+        <Redirect to="/admin/login" />
+      </>
     );
+  }
+
+  //if (user && !isLoading) {
+    return (
+      <Layout>
+        <MenuSider menuCollapsed={menuCollapsed} />
+        <Layout
+          className="layout-admin"
+          style={{ marginLeft: menuCollapsed ? "80px" : "200px" }}
+        >
+          <Header className="layout-admin__header">
+            <MenuTop
+              menuCollapsed={menuCollapsed}
+              setMenuCollapsed={setMenuCollapsed}
+            />
+          </Header>
+
+          <Content className="layout-admin__content">
+            <LoadRoutes routes={routes} />
+          </Content>
+
+          <Footer className="layout-admin__footer">
+            David Gerardo Martínez
+          </Footer>
+        </Layout>
+      </Layout>
+    );
+  //}
 }
 
 
