7d2a5341e981b21070e4c0a7d9718da681f23b59
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-04 19:25:09 -0600

    Añadido estilo al sign in

--- src/pages/Admin/SignIn/SignIn.js
+++ src/pages/Admin/SignIn/SignIn.js
@@ -7,14 +7,26 @@ import "./SignIn.scss";
 
 export default function SignIn (){
     const {Content} = Layout;
-    const {TabPanel} = Tabs;
+    const {TabPane} = Tabs;
 
     return (
         <Layout className="sign-in">
             <Content className="sign-in__content">
+        
                 <h1 className="sing-in__content-logo">
                     <img src={Logo} alt="David Gerardo Martinez"/>
                 </h1>
+
+                <div className="sign-in__content-tabs">
+                    <Tabs type="card">
+                        <TabPane tab={<span>Entrar</span>} key="1"> 
+                            Componente Login Form
+                        </TabPane>
+                        <TabPane tab={<span>Nuevo Usuario</span>} key="2">
+                            Component Register
+                        </TabPane>
+                    </Tabs>
+                </div>
             </Content>
         </Layout>
     );
