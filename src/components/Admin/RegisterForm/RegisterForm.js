39d264940466b9d9a162eb21599e310befb9fa19
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-04 19:48:49 -0600

    Creado slos items del RegisterForm

--- src/components/Admin/RegisterForm/RegisterForm.js
+++ src/components/Admin/RegisterForm/RegisterForm.js
@@ -1,5 +1,55 @@
 import React from 'react';
+import { Form, Input, Button, Checkbox, notification } from 'antd';
+import {MailOutlined, LockOutlined} from "@ant-design/icons";
+
+import './RegisterForm.scss';
 
 export default function RegisterForm() {
-    return 'Formulario de nuevo usuario';
+    return (
+        <Form className="register-form">
+
+            <Form.Item>
+                <Input 
+                    prefix={<MailOutlined style={{color: "rgba(0,0,0,.25)"}}/>}
+                    type="email"
+                    name="email"
+                    placeholder="Correo Electronico"
+                    className="register-form__input"
+                />
+            </Form.Item>
+
+            <Form.Item>
+            <Input 
+                    prefix={<LockOutlined style={{color: "rgba(0,0,0,.25)"}}/>}
+                    type="password"
+                    name="password"
+                    placeholder="Contraseña"
+                    className="register-form__input"
+                />                
+            </Form.Item>
+
+            <Form.Item>
+                <Input
+                    prefix={<LockOutlined style={{color: "rgba(0,0,0,.25)"}}/>}
+                    type="password"
+                    name="repeatPassword"
+                    placeholder="Ingresar nuevamente la contraseña"
+                    className="register-form__input"
+                />
+            </Form.Item>
+
+            <Form.Item>
+                <Checkbox name="privacyPolicy">
+                    He leído y acepto la política de privacidad.
+                </Checkbox>
+            </Form.Item>
+
+            <Form.Item>
+                <Button>
+                    Crear Cuenta
+                </Button>
+            </Form.Item>
+
+        </Form>
+    );
 };
--- src/pages/Admin/SignIn/SignIn.js
+++ src/pages/Admin/SignIn/SignIn.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Layout, Tabs} from "antd";
 import { Redirect } from "react-router-dom";
 import  Logo  from "../../../assets/img/png/logo-white.png";
+import RegisterForm from "../../../components/Admin/RegisterForm";
 
 import "./SignIn.scss";
 
@@ -23,7 +24,7 @@ export default function SignIn (){
                             Componente Login Form
                         </TabPane>
                         <TabPane tab={<span>Nuevo Usuario</span>} key="2">
-                            Component Register
+                            <RegisterForm />
                         </TabPane>
                     </Tabs>
                 </div>
