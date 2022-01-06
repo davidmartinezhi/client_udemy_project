620d6fffc464a2b13a97d791464d8d3dc8980836
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-05 21:10:29 -0600

    Llenado de formulario y teniendo acceso a los valores, completado

--- src/components/Admin/RegisterForm/RegisterForm.js
+++ src/components/Admin/RegisterForm/RegisterForm.js
@@ -15,8 +15,42 @@ export default function RegisterForm() {
     });
     //Para que cambie de valor, a los inputs se les debe poner un atributo onChange()
 
+
+    //e significa event, el evento que ocurre para el onChange
+    const changeForm = e => {
+        //Función que actualiza el forms cuando el usuario interactua con ella
+        //Para diferenciar entre el tipo de input que tengo (input y checkbox)
+        //Debo checar el name, privacyPolicy es mi checkbox y si es así, la trato como checkbox
+
+        //Si el input es en el checkbox
+        if(e.target.name === "privacyPolicy"){
+            //Agarro el objeto de los inputs, que en default es vacío
+            setInputs({
+                //Todos los inpus los dejo igual
+                ...inputs,
+
+                //El input en el que estamos actuando, que en este caso es el checkbox
+                //Lo checamos
+                //Para sacar valor de checkbox, se escribe .checked
+                [e.target.name]: e.target.checked,
+            });
+        }
+        //Si el input es normal
+        else{
+            setInputs({
+                ...inputs,
+                //Para sacar valor de un input es .value
+                [e.target.name]: e.target.value,
+            });
+        }
+    };
+
+    const register = () => {
+        console.log(inputs);
+    };
+
     return (
-        <Form className="register-form">
+        <Form className="register-form" onFinish={register} onChange={changeForm}>
 
             <Form.Item>
                 <Input 
