31807d24c0650bda426ddbe1ab05dc2e147e19d2
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-06 18:26:31 -0600

    Creada la funcion miLengthValidation

--- src/utils/formValidation.js
+++ src/utils/formValidation.js
@@ -2,7 +2,17 @@ export function minLengthValidation(inputData, minLength) {
     //Valida longitud del input
     const { value } = inputData;
 
-};
+    removeClassErrorSuccess(inputData);
+
+    if(value.length >= minLength){
+        inputData.classList.add('success');
+        return true;
+    } else {
+        inputData.classList.add('error');
+        return false;
+    }
+
+}
 
 
 
@@ -10,6 +20,6 @@ export function minLengthValidation(inputData, minLength) {
 function removeClassErrorSuccess(inputData){
     //Remueve la clase de error y success, en el input que le llega
     //Antes de validar algo, primero vamos a remover esas clases
-    inputData.classlist.remove('success');
-    inputData.classlist.remove('success');
+    inputData.classList.remove('success');
+    inputData.classList.remove('error');
 }
