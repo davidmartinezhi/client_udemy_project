93891ce447de600d477460a74e5e21702dcdea7e
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-07 20:36:45 -0600

    Creados parametros para endpoint en signUpApi

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,29 @@
 import {basePath, apiVersion} from "./config";
 
-export function signUpApi(){
+export function signUpApi( data ){
     //Función que nos da la dirección del sign-up
     const url = `${basePath}/${apiVersion}/sign_up`;
+
+    //Parametros del end-point
+    const params = {
+        
+        method: "POST", //Tipo de metodo
+        body: JSON.stringify(data), //Transforma la data
+
+        headers: {  //Headers de la petición
+            "Content-Type": "application/json"
+        }
+    }
+    console.log(data);
+    //fetch(url, params)
+        //.then(response => {
+        //   console.log(response);
+            //return response.json();
+        //});
+        //.then(result => {
+        //    return result;
+        //})
+
+        
 }
+
