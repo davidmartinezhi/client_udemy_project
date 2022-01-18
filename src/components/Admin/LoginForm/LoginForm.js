8bc1f22b1d1909a6588f0406d6fe2c0b718c5861
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-17 19:56:56 -0600

    Creado input para el correo electronico

--- src/components/Admin/LoginForm/LoginForm.js
+++ src/components/Admin/LoginForm/LoginForm.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+//Ant-Design
+import { Form, Input, Button, notification} from "antd";
+import {UserOutlined} from "@ant-design/icons";
+
+//Scss stylesheet
 import "./LoginForm.scss";
 
 export default function LoginForm(){
-    return("Login Form Component");
+    return(
+        <Form className="login-form">
+            <Form.Item>
+                <Input 
+                  prefix = {<UserOutlined style={{color: "rgba(0,0,0,.25)"}}/>}
+                  type = "email"
+                  name = "email"
+                  placeholder = "Correo Electronico"
+                  className = "login-form__input"
+                />
+            </Form.Item>
+        </Form>
+    );
 }
