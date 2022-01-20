3b63366ed3a3c109beb561b979cbd00479e42e5d
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-20 13:57:51 -0600

    Nombres contantes en las variables del proyecto

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+//Constants sirven para cuando tengamos que entrar al local storage
+//Utilizamos estas constantes, esto es por si algún día queremos cambiarle el nombre al accessToken, no tener que cambiarlo en todo el proyecto
+export const ACCESS_TOKEN = "accessToken";
+export const REFRESH_TOKEN = "refreshToken";
