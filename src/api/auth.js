1b649e6f79b303f18c45e9a2cf219941ca347dc1
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-20 14:45:03 -0600

    Definida funcion getAccessToken

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,2 +1,13 @@
 //Gestiona los Tokens y checa que el Token no haya caducado en el momento que se haga una petición
 
+//Importando constantes
+import { basePath, apiVersion } from "./config";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../utils/constants";
+
+//Paquete para decodificar token
+import jwtDecode from "jwt-decode";
+
+export function getAccessToken () {
+    //Retorna el Access Token
+    return "Ten el access Token";
+}
