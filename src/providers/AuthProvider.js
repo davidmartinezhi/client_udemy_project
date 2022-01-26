8836f9e7053068acc33883f74cc669c51ab8c573
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-25 20:55:22 -0600

    valida que el usuario este loggeado

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -16,5 +16,45 @@ export default function AuthProvider(props) {
       isLoading: true,
   });
 
+  useEffect(() => {
+    checkUserLogin(setUser);
+  }, []);
+
   return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+}
+
+function checkUserLogin(setUser){
+//Checa que el usuario esté logeado
+    
+    const accessToken = getAccessTokenApi();
+
+    //Si no existe el access token o ha caducado
+    if(!accessToken){
+        const refreshToken = getAccessTokenApi();
+
+        //Checo si el refreshToken también ha caducado
+        if(!refreshToken){
+            logout();   //Desloggeamos al usuario
+
+            setUser({   
+                user: null, //Desactivamos al usuario
+                isLoading: false //Avisamos que ya se termino de cargar la pagina
+            });
+        } 
+        //Si aún no caduca el refreshToken
+        else{
+            //Le damos un refresh al Token activo, para que cambie constantemente
+            //Esto para mayor seguridad
+            refreshAccessTokenApi(refreshToken);
+        }
+
+    }
+    //Si aún es valido el accessToken
+    else{
+        setUser({
+            user: accessToken,
+            isLoading: false
+        });
+    }
+
 }
