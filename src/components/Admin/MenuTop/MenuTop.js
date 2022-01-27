2c90822a5a39c0837536c5e85688040b9a9db360
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-26 18:54:29 -0600

    Agregada funcionalidad al boton de logOut

--- src/components/Admin/MenuTop/MenuTop.js
+++ src/components/Admin/MenuTop/MenuTop.js
@@ -4,32 +4,35 @@ import { MenuUnfoldOutlined, MenuFoldOutlined, PoweroffOutlined } from '@ant-des
 import AgusLogo from '../../../assets/img/png/white_logo_transparent_background copy.png';
 import "./MenuTop.scss";
 import { Link } from 'react-router-dom';
+import { logout } from '../../../api/auth';
 
-export default function MenuTop( props ){
-    
-    const {menuCollapsed, setMenuCollapsed} = props;
+export default function MenuTop(props) {
+  const { menuCollapsed, setMenuCollapsed } = props;
+  const logoutUser = () => {
+    logout();
+    window.location.reload();
+  };
 
-    return (
-        <div className="menu-top">
-            <div className="menu-top__left">
-                <Link to="/admin">
-                    <img 
-                    className="menu-top__left-logo"
-                    src={AgusLogo}
-                    alt="David Gerardo Martínez"
-                    />
-                </Link>
-                <Button type="link" onClick={() => setMenuCollapsed(!menuCollapsed)} >
-                    {menuCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined/>}
-                    
-                </Button>
-            </div>
+  return (
+    <div className="menu-top">
+      <div className="menu-top__left">
+        <Link to="/admin">
+          <img
+            className="menu-top__left-logo"
+            src={AgusLogo}
+            alt="David Gerardo Martínez"
+          />
+        </Link>
+        <Button type="link" onClick={() => setMenuCollapsed(!menuCollapsed)}>
+          {menuCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        </Button>
+      </div>
 
-            <div className="menu-top__right">
-                <Button type="link" onClick={() => console.log("Desconexion...")}>
-                    <PoweroffOutlined/>
-                </Button>
-            </div>
-        </div>
-    );
+      <div className="menu-top__right">
+        <Button type="link" onClick={logoutUser}>
+            <PoweroffOutlined/>
+        </Button>
+      </div>
+    </div>
+  );
 }
