f7824ad25032a34323260f73a66441780b033a29
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-27 19:27:46 -0600

    Actualizado menu de navegación

--- src/components/Admin/MenuSider/MenuSider.js
+++ src/components/Admin/MenuSider/MenuSider.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import {Layout, Menu } from 'antd';
-import { HomeOutlined, MenuOutlined  } from '@ant-design/icons';
+import { HomeOutlined, GroupOutlined  } from '@ant-design/icons';
 
 import './MenuSider.scss';
 
-export default function MenuSider ( props ) {
+function MenuSider ( props ) {
     const { Sider } = Layout;
     const { Item } = Menu;
 
-    const { menuCollapsed} = props;
+    const { menuCollapsed, location } = props;
 
     return(
         <Sider className="admin-sider" collapsed={menuCollapsed}>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={[location.pathname]}>
                 {/*  Home  */}
-                <Item key='1'>
+                <Item key='/admin'>
                     <Link to={"/admin"}>
                         <HomeOutlined />
                         <span className="nav-text">Home</span>
@@ -23,13 +23,15 @@ export default function MenuSider ( props ) {
                 </Item>
 
                 {/*  */}
-                <Item key='2'>
-                    <Link to="/admin/menu-web">
-                        <MenuOutlined />
-                        <span className="nav-text"> Menu Web </span>
+                <Item key='/admin/users'>
+                    <Link to="/admin/users">
+                        <GroupOutlined />
+                        <span className="nav-text"> Usuarios </span>
                     </Link>
                 </Item>
             </Menu>
         </Sider>
     );
 }
+
+export default withRouter(MenuSider);
