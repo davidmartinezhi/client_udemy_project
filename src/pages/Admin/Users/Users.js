c5e7f9bb50a25837bce73a917b124ffe6cd64452
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-27 19:09:09 -0600

    inisital setup users

---
+++ src/pages/Admin/Users/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Users from '.';
+
+import "./Users.scss";
+
+export default function Users(){
+    return(
+        <div>
+            <h1>Lista de usuarios</h1>
+        </div>
+    )
+}
