6db78936c82e2b5d1aebd8d6cbbb269fd35a06eb
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-01-28 20:22:53 -0600

    creada funcion para recibir usuarios activos

--- src/api/user.js
+++ src/api/user.js
@@ -94,3 +94,26 @@ export function getUsersApi( token ) {
     return err.message;
   });
 }
+
+export function getUsersActiveApi( token , status) {
+  const url = `${basePath}/${apiVersion}/users-active?active=${status}`;
+
+  const params = {
+    method: "GET",
+    headers: {
+      "Content-Type": "Application/json",
+      Authorization: token
+    }
+  };
+
+  return fetch(url, params)
+  .then( response => {
+    return response.json();
+  })
+  .then( result => {
+    return result;
+  })
+  .catch(err => {
+    return err.message;
+  });
+}
