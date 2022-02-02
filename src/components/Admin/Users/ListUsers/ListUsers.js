e051a2d9a1165ef1645fa4281b19af091fdce199
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-02-01 20:27:38 -0600

    Se despliegan los usuarios activos en la pantalla

--- src/components/Admin/Users/ListUsers/ListUsers.js
+++ src/components/Admin/Users/ListUsers/ListUsers.js
@@ -1,35 +1,56 @@
 import React, { useState } from 'react';
-import { Switch, List, Avatar, Button, Icon} from 'antd';
+import { Switch, List, Avatar, Button } from "antd";
 
-import NoAvatar from '../../../../assets/img/png/no-avatar.png';
+import NoAvatar from "../../../../assets/img/png/no-avatar.png";
 
-import './ListUsers.scss';
+import "./ListUsers.scss";
 
-export default function ListUsers(props){
+export default function ListUsers(props) {
+  const { usersActive, usersInactive } = props;
+  const [viewUsersActive, setViewUsersActive] = useState(true);
 
-    const { usersActive, usersInactive } = props;
-    const [viewUsersActive, setViewUsersActive] = useState(true);
-
-    return(
-        <div className='list-users'>
-            <div className='list-users__switch'>
-                <Switch
-                    defaultChecked
-                    onChange={ () => setViewUsersActive(!viewUsersActive)}
-                />
-                <span>
-                    {viewUsersActive ? "Usuarios Activos" : "Usuarios Inactivos"}
-                </span>
-            </div>
-            {viewUsersActive ? <UsersActive/> : <UsersInactive/>}
-        </div>
-    );
+  return (
+    <div className="list-users">
+      <div className="list-users__switch">
+        <Switch
+          defaultChecked
+          onChange={() => setViewUsersActive(!viewUsersActive)}
+        />
+        <span>
+          {viewUsersActive ? "Usuarios Activos" : "Usuarios Inactivos"}
+        </span>
+      </div>
+      {viewUsersActive ? (
+        <UserActive usersActive={usersActive} />
+      ) : (
+        <UserInactive usersInactive={usersInactive}/>
+      )}
+    </div>
+  );
 }
 
-function UsersActive(){
-    return (<h3>Lista de Usuarios Activos</h3>);
+function UserActive(props) {
+  const { usersActive } = props;
+  
+  return (
+    <List
+    className="users-active"
+    itemLayout="horizontal"
+    dataSource={usersActive}
+    renderItem={user => (
+      <List.Item>
+        <List.Item.Meta
+          avatar={<Avatar src={user.avatar ? user.avatar : NoAvatar} />}
+          title={`${user.name ? user.name : "..."} ${user.lastname ? user.lastname : "..."}`}
+          description={user.email}
+        />
+      </List.Item>
+    )}
+  />
+  );
 }
 
-function UsersInactive(){
-    return (<h3>Lista de Usuarios Inactivos</h3>);
+function UserInactive(props) {
+  const { usersInactive } = props;
+  return <h3>Lista de Usuarios Inactivos</h3>;
 }
