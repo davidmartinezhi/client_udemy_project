1ab17b4d826b004c18895c56ef46dd781a9e136f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-02-01 19:22:56 -0600

    Importaciones basicas

---
+++ src/components/Admin/Users/ListUsers/ListUsers.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { Switch, List, Avatar, Button, Icon} from 'antd';
+
+import NoAvatar from '../../../../assets/img/png/no-avatar.png';
+
+import './ListUsers.scss';
