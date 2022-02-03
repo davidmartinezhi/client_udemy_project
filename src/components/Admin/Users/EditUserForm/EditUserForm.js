d289715ed1b3dac7f97f7c6e7d640d2ab3d333f6
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-02-02 20:16:52 -0600

    Initial setup EditUserForm

---
+++ src/components/Admin/Users/EditUserForm/EditUserForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "./EditUserForm.scss";
+
+export default function EditUserForm(props){
+
+    const { user } = props;
+
+    return(
+        <div>
+            <h1>Formulario de edición de usuario</h1>
+            <h2>{user.email}</h2>
+        </div>
+    );
+}
