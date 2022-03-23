026b6889b27d3cf272fbdf60fbc4f67e3b36a08f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-03-22 18:11:37 -0600

    Creada pagina menuweb

---
+++ src/pages/Admin/MenuWeb/MenuWeb.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//import "./MenuWeb.scss"
+
+export default function MenuWeb() {
+    return (
+        <div className='menu-web'>
+            <h1>Menu Web</h1>
+        </div>
+    );
+}
