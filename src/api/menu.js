4e4a380a92cc1bd5c83d882b85f2af143358580a
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-05 16:17:22 -0500

    Creada api para actualizar los menús

--- src/api/menu.js
+++ src/api/menu.js
@@ -18,3 +18,28 @@ export function getMenuApi(){
             return error.message;
         })
 }
+
+//Actualizamos el menú
+export function updateMenuApi(token, menuId, data) {
+  const url = `${basePath}/${apiVersion}/update-menu/${menuId}`;
+
+  const params = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify(data),
+  };
+
+  return fetch(url, params)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      return result.message;
+    })
+    .catch((err) => {
+      return err.message;
+    });
+}
