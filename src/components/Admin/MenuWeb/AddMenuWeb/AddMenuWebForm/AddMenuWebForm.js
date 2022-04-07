857a6de7b61f12b593f1c01acd07ad24ba36a09f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-06 19:56:33 -0500

    initial setup formulario para form

---
+++ src/components/Admin/MenuWeb/AddMenuWeb/AddMenuWebForm/AddMenuWebForm.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import { Form, Input, Button, Select, notification} from "antd";
+import "./AddMenuWebForm.scss";
+
+export default function AddMenuWebForm(  ) {
+  return (
+    <div className='add-menu-web-form'>
+        <h2>AddMenuWebForm</h2>
+    </div>
+  );
+}
