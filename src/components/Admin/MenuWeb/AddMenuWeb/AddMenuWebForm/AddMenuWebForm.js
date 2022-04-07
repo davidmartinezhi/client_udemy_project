a00b68770a9d4addbead56e23d69e396d32ef8a1
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-06 20:19:13 -0500

    Estructura del formulario para menu

--- src/components/Admin/MenuWeb/AddMenuWeb/AddMenuWebForm/AddMenuWebForm.js
+++ src/components/Admin/MenuWeb/AddMenuWeb/AddMenuWebForm/AddMenuWebForm.js
@@ -1,11 +1,53 @@
 import React, {useState} from 'react';
 import { Form, Input, Button, Select, notification} from "antd";
+import {FontSizeOutlined} from "@ant-design/icons";
 import "./AddMenuWebForm.scss";
 
 export default function AddMenuWebForm(  ) {
   return (
     <div className='add-menu-web-form'>
-        <h2>AddMenuWebForm</h2>
+        <AddForm />
     </div>
   );
+}
+
+function AddForm( props ){
+
+    const { Option } = Select;
+    const selectBefore = (
+        <Select
+            defaultValue="http://"
+            style={{ width: 90 }}
+        >
+            <Option value="http://">http://</Option>
+            <Option value="https://">https://</Option>
+        </Select>
+    );
+
+    return (
+        <Form className="form-add">
+            <Form.Item>
+                <Input 
+                    prefix={<FontSizeOutlined />}
+                    placeholder="Titulo"
+                    //value={}
+                    //onChange={}
+                />
+            </Form.Item>
+
+            <Form.Item>
+                <Input 
+                    addonBefore={selectBefore}
+                    placeholder="URL"
+                    //value={}
+                    //onChange={}
+                />
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="btn-submit">
+                    Crear Menú
+                </Button>
+            </Form.Item>
+        </Form>
+    );
 }
