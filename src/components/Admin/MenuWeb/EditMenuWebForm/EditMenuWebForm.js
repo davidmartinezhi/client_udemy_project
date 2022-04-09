f571a15271744632d84cced866d6a803d5f20e3b
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-08 20:37:11 -0500

    Actualización del menu en la base de datos

--- src/components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.js
+++ src/components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.js
@@ -1,51 +1,87 @@
-import React, {useState, useEffect} from "react"
-import { Form, Input, Button, notification} from 'antd'
-import {FontSizeOutlined, LinkOutlined} from '@ant-design/icons'
-import { updateMenuApi } from '../../../../api/menu'
-import { getAccessTokenApi } from '../../../../api/auth'
-import "./EditMenuWebForm.scss"
+import React, { useState, useEffect } from "react";
+import { Form, Input, Button, notification } from "antd";
+import { FontSizeOutlined, LinkOutlined } from "@ant-design/icons";
+import { updateMenuApi } from "../../../../api/menu";
+import { getAccessTokenApi } from "../../../../api/auth";
+import "./EditMenuWebForm.scss";
 
-export default function EditMenuWebForm( props ) {
+export default function EditMenuWebForm(props) {
+  const { setIsVisibleModal, setReloadMenuWeb, menu } = props;
+  const [menuWebData, setMenuWebData] = useState({});
 
-    const { setIsVisibleModal, setReloadMenuWeb, menu } = props;
+  //Hace que menuWebData sea el valor actual del menú actual
+  useEffect(() => {
+    setMenuWebData(menu);
+  }, [menu]);
+
+  //Actualiza la info en la base de datos
+  const editMenu = (e) => {
+    if (!menuWebData.title || !menuWebData.url) {
+      notification["error"]({
+        message: "Todos los campos son obligatorios",
+      });
+    } else {
+      //Sacamos el access token del usuario
+      const accessToken = getAccessTokenApi();
+
+      //Mando el access token, el id del menú a modificar y la info del nuevo menú
+      updateMenuApi(accessToken, menuWebData._id, menuWebData)
+        .then((response) => {
+          notification["success"]({ message: response });
+          setIsVisibleModal(false);
+          setReloadMenuWeb(true);
+        })
+        .catch(() => {
+          notification["error"]({
+            message: "Error del servidor, intentelo más tarde.",
+          });
+        });
+    }
+  };
 
   return (
     <div className="edit-menu-web-form">
-        <EditForm />
+      <EditForm
+        menuWebData={menuWebData}
+        setMenuWebData={setMenuWebData}
+        editMenu={editMenu}
+      />
     </div>
   );
 }
 
+function EditForm(props) {
+  const { menuWebData, setMenuWebData, editMenu } = props;
 
-function EditForm( props ){
-    
-    // const {menuWebData, setMenuWebData, editMenu, menu} = props;
+  return (
+    <Form className="form-edit" onFinish={editMenu}>
+      <Form.Item>
+        <Input
+          prefix={<FontSizeOutlined />}
+          placeholder="Titulo"
+          value={menuWebData.title}
+          onChange={(e) =>
+            setMenuWebData({ ...menuWebData, title: e.target.value })
+          }
+        />
+      </Form.Item>
 
-    return(
-        <Form className="form-edit">
-            <Form.Item>
-                <Input 
-                    prefix={<FontSizeOutlined />}
-                    placeholder="Titulo"
-                    //value={}
-                    //onChange={}
-                />
-            </Form.Item>
+      <Form.Item>
+        <Input
+          prefix={<LinkOutlined />}
+          placeholder="URL"
+          value={menuWebData.url}
+          onChange={(e) =>
+            setMenuWebData({ ...menuWebData, url: e.target.value })
+          }
+        />
+      </Form.Item>
 
-            <Form.Item>
-                <Input 
-                    prefix={<LinkOutlined />}
-                    placeholder="URL"
-                    //value={}
-                    //onChange={}                    
-                />
-            </Form.Item>
-
-            <Form.Item>
-                <Button type="primary" htmlType="submit" className="btn-submit">
-                    Actualizar Menú
-                </Button>
-            </Form.Item>
-        </Form>
-    );
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="btn-submit">
+          Actualizar Menú
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
