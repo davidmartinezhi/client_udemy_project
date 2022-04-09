ebc7a91c9b739286082b1ea82eaf779603e9d3f3
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-08 20:20:02 -0500

    Form para editar un menú terminada

--- src/components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.js
+++ src/components/Admin/MenuWeb/EditMenuWebForm/EditMenuWebForm.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react"
 import { Form, Input, Button, notification} from 'antd'
-import {} from '@ant-design/icons'
+import {FontSizeOutlined, LinkOutlined} from '@ant-design/icons'
 import { updateMenuApi } from '../../../../api/menu'
 import { getAccessTokenApi } from '../../../../api/auth'
 import "./EditMenuWebForm.scss"
@@ -10,8 +10,42 @@ export default function EditMenuWebForm( props ) {
     const { setIsVisibleModal, setReloadMenuWeb, menu } = props;
 
   return (
-    <div>
-        <h1>Editando el menú {menu.title}</h1>
+    <div className="edit-menu-web-form">
+        <EditForm />
     </div>
-  )
+  );
+}
+
+
+function EditForm( props ){
+    
+    // const {menuWebData, setMenuWebData, editMenu, menu} = props;
+
+    return(
+        <Form className="form-edit">
+            <Form.Item>
+                <Input 
+                    prefix={<FontSizeOutlined />}
+                    placeholder="Titulo"
+                    //value={}
+                    //onChange={}
+                />
+            </Form.Item>
+
+            <Form.Item>
+                <Input 
+                    prefix={<LinkOutlined />}
+                    placeholder="URL"
+                    //value={}
+                    //onChange={}                    
+                />
+            </Form.Item>
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="btn-submit">
+                    Actualizar Menú
+                </Button>
+            </Form.Item>
+        </Form>
+    );
 }
