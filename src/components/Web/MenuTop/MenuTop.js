c6dd152b1d8a572b469a57bebe6e84391f55c0f8
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-12 01:24:08 -0500

    Agregado contenido basico y estilo

--- src/components/Web/MenuTop/MenuTop.js
+++ src/components/Web/MenuTop/MenuTop.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Menu} from 'antd'
+import {Link} from 'react-router-dom'
+import './MenuTop.scss'
 
 export default function MenuTop() {
   return (
-    <div>
-        <h1>Menu Top...</h1>
-    </div>
+    <Menu className='menu-top' mode='horizontal'>
+        <Menu.Item className='menu-top__logo'>
+            Logo...
+        </Menu.Item>
+
+        <Menu.Item className='menu-top__item'>
+            <Link to={"/"}>Home</Link>
+        </Menu.Item>
+        <Menu.Item className='menu-top__item'>
+            <Link to={"/contact"}>Contacto</Link>
+        </Menu.Item>
+
+        <div>Social Media...</div>
+    </Menu>
   )
 }
