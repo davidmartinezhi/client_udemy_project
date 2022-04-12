e8c2588e5f1534ac01b305babcf891531a86d86f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-12 01:03:51 -0500

    Creado layout para pagina publica

--- src/layout/LayoutBasic.js
+++ src/layout/LayoutBasic.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route , Switch } from 'react-router-dom';
-import {Layout} from 'antd';
+import {Layout, Row, Col} from 'antd';
 
 import './LayoutBasic.scss';
 
@@ -8,19 +8,39 @@ export default function LayoutBasic( props ){
     const { routes } = props;   //Sistema de rutas que me llega por props
     const { Content , Footer } = Layout; //Para usar de manera directa esos componentes
 
-    return(
-        <Layout>
-            <h2>Menu...</h2>
-            <Layout>
-                <Content>
-                    <LoadRoutes routes={routes} />
-                </Content>
+    /* 
+    Columnas md 4, md 16 y md 4
+    Ayuda a que en mobil, abarque la pantalla completa, pero tenga padding en pantalla media y grande
+    Para que ocupe la pantalla completa en tablet, sería con lg
+    */
+    return (
+        <Row>
+            <Col md={4} />
+            <Col md={16}>
+                <p>MenuTop</p>
+                <LoadRoutes routes={routes} />
                 <Footer>
                     David Gerardo Martínez
                 </Footer>
-            </Layout>
-        </Layout>
+            </Col>
+            <Col md={4} />
+            
+        </Row>
     );
+
+    // return(
+    //     <Layout>
+    //         <h2>Menu...</h2>
+    //         <Layout>
+    //             <Content>
+    //                 <LoadRoutes routes={routes} />
+    //             </Content>
+    //             <Footer>
+    //                 David Gerardo Martínez
+    //             </Footer>
+    //         </Layout>
+    //     </Layout>
+    // );
 }
 
 
