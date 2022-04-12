b8ef2f589c2507573aefc9b643cfa6ff3e049ed2
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-12 01:28:52 -0500

    Importado logo y agregado link a home

--- src/components/Web/MenuTop/MenuTop.js
+++ src/components/Web/MenuTop/MenuTop.js
@@ -1,13 +1,16 @@
 import React, {useState} from 'react'
 import {Menu} from 'antd'
 import {Link} from 'react-router-dom'
+import logoWhite from '../../../assets/img/png/white_logo_transparent_background copy.png'
 import './MenuTop.scss'
 
 export default function MenuTop() {
   return (
     <Menu className='menu-top' mode='horizontal'>
         <Menu.Item className='menu-top__logo'>
-            Logo...
+            <Link to={'/'}>
+                <img src={logoWhite} alt="David Gerardo Martínez Hidrogo" />
+            </Link>
         </Menu.Item>
 
         <Menu.Item className='menu-top__item'>
