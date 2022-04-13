a64861c4494f962cc5843c77e5fd1383326387a1
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-12 20:20:16 -0500

    Actualizado nombre de la clase

--- src/components/Web/MenuTop/MenuTop.js
+++ src/components/Web/MenuTop/MenuTop.js
@@ -1,22 +1,33 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Menu} from 'antd'
 import {Link} from 'react-router-dom'
+import {getMenuApi} from '../../../api/menu'
 import logoWhite from '../../../assets/img/png/white_logo_transparent_background copy.png'
 import './MenuTop.scss'
 
 export default function MenuTop() {
+
+    const [menuData, setMenuData] = useState([]);
+
+    useEffect(() => {
+      getMenuApi().then( response => {
+          console.log(response);
+      })
+    }, [])
+    
+
   return (
-    <Menu className='menu-top' mode='horizontal'>
-        <Menu.Item className='menu-top__logo'>
+    <Menu className='menu-top-web' mode='horizontal'>
+        <Menu.Item className='menu-top-web__logo'>
             <Link to={'/'}>
                 <img src={logoWhite} alt="David Gerardo Martínez Hidrogo" />
             </Link>
         </Menu.Item>
 
-        <Menu.Item className='menu-top__item'>
+        <Menu.Item className='menu-top-web__item'>
             <Link to={"/"}>Home</Link>
         </Menu.Item>
-        <Menu.Item className='menu-top__item'>
+        <Menu.Item className='menu-top-web__item'>
             <Link to={"/contact"}>Contacto</Link>
         </Menu.Item>
 
