49993aa529cd6ba5e3fa3108221d3899bd976b6b
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-12 20:43:40 -0500

    Menú dinamico en la menuTop navbar

--- src/components/Web/MenuTop/MenuTop.js
+++ src/components/Web/MenuTop/MenuTop.js
@@ -9,29 +9,62 @@ export default function MenuTop() {
 
     const [menuData, setMenuData] = useState([]);
 
+    //console.log(menuData);
+
     useEffect(() => {
       getMenuApi().then( response => {
-          console.log(response);
-      })
+          let arrayMenu = [];
+          response.menusStored.forEach( item => {
+              if(item.active){
+                  arrayMenu.push(item);
+              }
+          });
+          setMenuData(arrayMenu);
+      });
     }, [])
     
 
   return (
-    <Menu className='menu-top-web' mode='horizontal'>
-        <Menu.Item className='menu-top-web__logo'>
-            <Link to={'/'}>
-                <img src={logoWhite} alt="David Gerardo Martínez Hidrogo" />
-            </Link>
-        </Menu.Item>
+    <Menu className="menu-top-web" mode="horizontal">
+      <Menu.Item className="menu-top-web__logo">
+        <Link to={"/"}>
+          <img src={logoWhite} alt="David Gerardo Martínez Hidrogo" />
+        </Link>
+      </Menu.Item>
 
-        <Menu.Item className='menu-top-web__item'>
+      {
+      menuData.map((item) => {
+        //Si el url tiene http en el texto, significa que es una ruta externa
+        //Entonces si tiene un -1, significa que no se encontro http dentro del string y es local
+        const external = item.url.indexOf("http") > -1 ? true : false;
+
+        if (external) {
+          return (
+            <Menu.Item key={item._id} className="menu-top-web__item">
+              <a href={item.url} target="_blank">{item.title}</a>
+            </Menu.Item>
+          );
+        }
+
+        return (
+            <Menu.Item
+                key={item._id}
+                className="menu-top-web__item"
+            >
+                <Link to={item.url}>{item.title}</Link>
+            </Menu.Item>
+        );
+      })
+      }
+
+      {/* <Menu.Item className='menu-top-web__item'>
             <Link to={"/"}>Home</Link>
         </Menu.Item>
         <Menu.Item className='menu-top-web__item'>
             <Link to={"/contact"}>Contacto</Link>
-        </Menu.Item>
+        </Menu.Item> */}
 
-        <div>Social Media...</div>
+      <div>Social Media...</div>
     </Menu>
-  )
+  );
 }
