d66f435c2b661cf7f6362f04bdc3acefa79ff0e4
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-15 15:32:31 -0500

    Menu collapsado terminado

--- src/components/Web/HomeCourses/HomeCourses.js
+++ src/components/Web/HomeCourses/HomeCourses.js
@@ -1,73 +1,62 @@
-import React from 'react'
-import {Row, Col, Card, Button} from 'antd'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Row, Col, Card, Button } from "antd";
+import { Link } from "react-router-dom";
 
 //Imagenes miniatura de los cursos
-import reactJsHooks from '../../../assets/img/jpg/react-js-hooks.jpg';
-import reactNative from '../../../assets/img/jpg/react-native.jpg';
-import javascript from '../../../assets/img/jpg/javascript-es6.jpg';
-import wordPress from '../../../assets/img/jpg/wordpress.jpg'
-import prestaShop from '../../../assets/img/jpg/prestashop-1-7.jpg'
-import cssGrid from '../../../assets/img/jpg/css-grid.jpg'
+import reactJsHooks from "../../../assets/img/jpg/react-js-hooks.jpg";
+import reactNative from "../../../assets/img/jpg/react-native.jpg";
+import javascript from "../../../assets/img/jpg/javascript-es6.jpg";
+import wordPress from "../../../assets/img/jpg/wordpress.jpg";
+import prestaShop from "../../../assets/img/jpg/prestashop-1-7.jpg";
+import cssGrid from "../../../assets/img/jpg/css-grid.jpg";
 
-import './HomeCourses.scss'
-
+import "./HomeCourses.scss";
 
 export default function HomeCourses() {
   return (
     <>
-        <Row className="home-courses">
-            <Col md={24} className="home-courses__title">
-                <h2>Aprende y mejora tus habilidades.</h2>
+      <Row className="home-courses">
+        <Col md={24} className="home-courses__title">
+          <h2>Aprende y mejora tus habilidades.</h2>
+        </Col>
+        <Col md={4}></Col>
+        <Col md={16}>
+          <Row className="row-courses">
+            <Col md={6}>
+              <CardCourses
+                image={reactJsHooks}
+                title="ReactJs Hooks"
+                subtitle="Intermedio - React/Javascript"
+                link="https://twitter.com/davidmtzhi"
+              />
             </Col>
-            <Col md={4}></Col>
-            <Col md={16}>
-                <Row className="row-courses">
-                    <Col md={6}>
-                        <CardCourses 
-                            image={reactJsHooks}
-                            title="ReactJs Hooks"
-                            subtitle="Intermedio - React/Javascript"
-                            link="https://twitter.com/davidmtzhi"
-                        />
-                    </Col>
-                    <Col md={6}>
-                        Curso...
-                    </Col>
-                    <Col md={6}>
-                        Curso...
-                    </Col>
-                    <Col md={6}>
-                        Curso...
-                    </Col>
-                    <Col md={6}>
-                        Curso...
-                    </Col>
-                    <Col md={6}>
-                        Curso...
-                    </Col>
-                </Row>
-            </Col>
-            <Col md={4}></Col>
-        </Row>
+            <Col md={6}>Curso...</Col>
+            <Col md={6}>Curso...</Col>
+            <Col md={6}>Curso...</Col>
+            <Col md={6}>Curso...</Col>
+            <Col md={6}>Curso...</Col>
+          </Row>
+        </Col>
+        <Col md={4}></Col>
+      </Row>
     </>
-  )
+  );
 }
 
-function CardCourses( props ){
-    const { image, title, subtitle, link } = props;
+function CardCourses(props) {
+  const { image, title, subtitle, link } = props;
 
-    const {Meta} = Card;
+  const { Meta } = Card;
 
-    return(
-        <a href={link} target="_blank">
-            <Card
-                className="gone-courses__card"
-                cover={<img src={image} alt={title}/>}
-                actions={[<Button>INGRESAR</Button>]}
-            >
-                <Meta title={title} description={subtitle} />
-            </Card>
-        </a>
-    );
+  return (
+    <a href={link} target="_blank">
+      <Card
+        className="gone-courses__card"
+        cover={<img src={image} alt={title} />}
+        actions={[<Button>INGRESAR</Button>]}
+      >
+        <Meta title={title} description={subtitle} />
+      </Card>
+    </a>
+  );
 }
--- src/components/Web/MenuTopMobile/MenuTopMobile.js
+++ src/components/Web/MenuTopMobile/MenuTopMobile.js
@@ -1,110 +1,98 @@
-import React, {useState, useEffect} from 'react'
-import ReactDOM from 'react-dom'
-import {Row, Col, Menu} from 'antd'
-import {Link} from 'react-router-dom'
-import SocialLinks from '../SocialLinks'
-import {getMenuApi} from '../../../api/menu'
-import { HamburgerArrow } from 'react-animated-burgers'
-import logoWhite from '../../../assets/img/png/white_logo_transparent_background copy.png'
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { Row, Col, Menu } from "antd";
+import { Link } from "react-router-dom";
+import SocialLinks from "../SocialLinks";
+import { getMenuApi } from "../../../api/menu";
+import { HamburgerArrow , HamburgerCollapse} from "react-animated-burgers";
 
-import './MenuTopMobile.scss'
+import "./MenuTopMobile.scss";
 
 export default function MenuTopMobile(props) {
-    
-    const {isActive, toggleButton} = props;
+  const { isActive, toggleButton } = props;
 
-    if(!isActive){
-        return (
-            <HamburgerArrow
-            buttonColor="black"
-            barColor="white"
-            {...{ isActive, toggleButton }}  
-            />         
-        );
-    }
-
-
-  return (
-
-    <>
+  if (!isActive) {
+    return (
         <Row>
-            <Col sm={24}>
-                <HamburgerArrow
-                    buttonColor="black"
-                    barColor="white"
-                    {...{ isActive, toggleButton }}
-                />
-            </Col>
+          <Col sm={24}>
+            <HamburgerCollapse         
+            buttonColor = "black"
+            barColor = "white"
+              {...{ isActive, toggleButton }}
+            />
+          </Col>
         </Row>
-        <MenuMobile /> 
-    </>
-
-  )
+    );
+  } else {
+    return (
+      <div>
+        <Row>
+          <Col sm={24}>
+            <HamburgerCollapse         
+            buttonColor = "black"
+            barColor = "white"
+              {...{ isActive, toggleButton }}
+            />
+          </Col>
+        </Row>
+        <MenuMobile />
+      </div>
+    );
+  }
 }
 
-function MenuMobile( props ){
+function MenuMobile() {
+  const [menuData, setMenuData] = useState([]);
 
-    const {isActive, toggleButton} = props;
-    const [menuData, setMenuData] = useState([]);
-    
-    useEffect(() => {
-        getMenuApi().then( response => {
-            let arrayMenu = [];
-            response.menusStored.forEach( item => {
-                if(item.active){
-                    arrayMenu.push(item);
-                }
-            });
-            setMenuData(arrayMenu);
-        });
-      }, [])
+  useEffect(() => {
+    getMenuApi().then((response) => {
+      let arrayMenu = [];
+      response.menusStored.forEach((item) => {
+        if (item.active) {
+          arrayMenu.push(item);
+        }
+      });
+      setMenuData(arrayMenu);
+    });
+  }, []);
 
-      return ReactDOM.createPortal(
-        <>
-        <Menu className="menu-top-web-mobile" mode="vertical">
-        <Menu.Item className="menu-top-webmobile__logo">
-          <Link to={"/"}>
-            <a href={"/"} target="_blank">Home</a>
-          </Link>
-        </Menu.Item>
-          <div className="menu-top-web-mobile__nav">
-          {
-              menuData.map((item) => {
-                  //Si el url tiene http en el texto, significa que es una ruta externa
-                  //Entonces si tiene un -1, significa que no se encontro http dentro del string y es local
-                  const external = item.url.indexOf("http") > -1 ? true : false;
-  
-                  if (external) {
-                  return (
-                      <Menu.Item key={item._id} className="menu-top-web-mobile__item">
-                      <a href={item.url} target="_blank">{item.title}</a>
-                      </Menu.Item>
-                  );
-                  }
-  
-                  return (
-                      <Menu.Item
-                          key={item._id}
-                          className="menu-top-web-mobile__item"
-                      >
-                          <Link to={item.url}>{item.title}</Link>
-                      </Menu.Item>
-                  );
-              })
-          }
-          </div>
-  
-  
+  return ReactDOM.createPortal(
+    <>
+      <Menu className="menu-top-web-mobile" mode="vertical" theme="none">
+        <div className="menu-top-web-mobile__nav">
+          {menuData.map((item) => {
+            //Si el url tiene http en el texto, significa que es una ruta externa
+            //Entonces si tiene un -1, significa que no se encontro http dentro del string y es local
+            const external = item.url.indexOf("http") > -1 ? true : false;
+
+            if (external) {
+              return (
+                <Menu.Item key={item._id} className="menu-top-web-mobile__item">
+                  <a href={item.url} target="_blank">
+                    {item.title}
+                  </a>
+                </Menu.Item>
+              );
+            }
+
+            return (
+              <Menu.Item key={item._id} className="menu-top-web-mobile__item">
+                <Link to={item.url}>{item.title}</Link>
+              </Menu.Item>
+            );
+          })}
+        </div>
+
         {/* <Menu.Item className='menu-top-web__item'>
               <Link to={"/"}>Home</Link>
           </Menu.Item>
           <Menu.Item className='menu-top-web__item'>
               <Link to={"/contact"}>Contacto</Link>
           </Menu.Item> */}
-  
-        <SocialLinks className="menu-top-web-mobile__social"/>
+
+        <SocialLinks className="menu-top-web-mobile__social" />
       </Menu>
-        </>,
-        document.getElementById('menuWebMobile')
-      );
+    </>,
+    document.getElementById("menuWebMobile")
+  );
 }
