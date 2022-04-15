9288a34b672499e143567abe00e3a3bc891ee659
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-14 20:02:29 -0500

    Agregado codigo a MainBanner

--- src/components/Web/MainBanner/MainBanner.js
+++ src/components/Web/MainBanner/MainBanner.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import './MainBanner.scss'
+import React from "react";
+import {Col, Row} from 'antd';
+import "./MainBanner.scss";
 
 export default function MainBanner() {
   return (
-    <div>
-        <h1>Main Banner</h1>
+    <div className="main-banner">
+      <div className="main-banner__dark">
+        <Row>
+          <Col md={4} />
+          <Col md={16}>
+            <h2>
+              Aprender nuevas <br /> tecnologías web y mobiles
+            </h2>
+            <h3>
+              A través de cursos practicos y actualizados, creados por <br />
+              profesionales con años de experiencia.
+            </h3>
+          </Col>
+          <Col md={4} />
+        </Row>
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,12 @@
 import React from "react";
-import {Col, Row} from 'antd';
 import MainBanner from "../components/Web/MainBanner";
+import HomeCourses from "../components/Web/HomeCourses";
 
 export default function Home(){
     return(
-        <div className="main-banner">
-            <div className="main-banner__dark">
-                <Row>
-                    <Col md={4} />
-                    <Col md={16}>
-                        <h2>Aprender nuevas <br/> tecnologías web y mobiles</h2>
-                        <h3>
-                            A través de cursos practicos y actualizados, creados por <br/>
-                            profesionales con años de experiencia.
-                        </h3>
-                    </Col>
-                    <Col md={4} />
-                </Row>
-            </div>
-        </div>
+        <>
+            <MainBanner />
+            <HomeCourses />
+        </>
     );
 }
