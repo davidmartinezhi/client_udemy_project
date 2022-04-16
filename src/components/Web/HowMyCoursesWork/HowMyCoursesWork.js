a752dd6a616a80cd85b343b11bb3c72faebc93c7
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-15 23:08:09 -0500

    Agregado titulo y estilo

--- src/components/Web/HowMyCoursesWork/HowMyCoursesWork.js
+++ src/components/Web/HowMyCoursesWork/HowMyCoursesWork.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Col, Row, Card} from 'antd'
+import {} from '@ant-design/icons'
 import './HowMyCoursesWork.scss'
 
 export default function HowMyCoursesWork() {
   return (
-    <div>HowMyCoursesWork</div>
+    <Row className='how-my-courses-work'>
+        <Col md={24} className='how-my-courses-work__title'>
+            <h2>¿Cómo funcionan mis cursos?</h2>
+            <h3>
+                Cada curso cuenta con contenido bajo la web de Udemy, 
+                activa las 24 horas del día los 365 días del año.
+            </h3>
+        </Col>
+    </Row>
   )
 }
