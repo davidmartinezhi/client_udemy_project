8f0fa58decf1c6d453e80a5d76c772b31ab7720c
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-15 23:19:14 -0500

    Creadas tarjetas

--- src/components/Web/HowMyCoursesWork/HowMyCoursesWork.js
+++ src/components/Web/HowMyCoursesWork/HowMyCoursesWork.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Col, Row, Card} from 'antd'
-import {} from '@ant-design/icons'
+import {ClockCircleOutlined, KeyOutlined, MessageOutlined} from '@ant-design/icons'
 import './HowMyCoursesWork.scss'
 
 export default function HowMyCoursesWork() {
@@ -13,6 +13,69 @@ export default function HowMyCoursesWork() {
                 activa las 24 horas del día los 365 días del año.
             </h3>
         </Col>
+        <Col md={4}></Col>
+        <Col md={16}>
+            <Row className="row-cards">
+                <Col md={8}>
+                    <CardInfo 
+                        icon={<ClockCircleOutlined />} 
+                        title="Cursos y Clases"
+                        subtitle="Cursos de entre 10 y 30 horas de contenido"
+                    />
+                </Col>
+                <Col md={8}>
+                    <CardInfo 
+                        icon={<KeyOutlined />} 
+                        title="Acceso 24/7"
+                        subtitle="Accede a los cursos en cualquier momento"
+                    />
+                </Col>
+                <Col md={8}>
+                    <CardInfo 
+                        icon={<MessageOutlined />} 
+                        title="Aprendizaje Colaborativo"
+                        subtitle="Aprende en conjunto al publicar tus preguntas"
+                    />
+                </Col>
+            </Row>
+            <Row className="row-cards">
+                <Col md={8}>
+                    <CardInfo 
+                        icon={<ClockCircleOutlined />} 
+                        title="Cursos y Clases"
+                        subtitle="Cursos de entre 10 y 30 horas de contenido"
+                    />
+                </Col>
+                <Col md={8}>
+                    <CardInfo 
+                        icon={<KeyOutlined />} 
+                        title="Acceso 24/7"
+                        subtitle="Accede a los cursos en cualquier momento"
+                    />
+                </Col>
+                <Col md={8}>
+                    <CardInfo 
+                        icon={<MessageOutlined />} 
+                        title="Aprendizaje Colaborativo"
+                        subtitle="Aprende en conjunto al publicar tus preguntas"
+                    />
+                </Col>
+            </Row>
+        </Col>
+        <Col md={4}></Col>
     </Row>
   )
+}
+
+function CardInfo( props ){
+    const {icon, title, subtitle} = props;
+    const {Meta} = Card;
+
+    return(
+        <Card className="how-my-courses-work__card">
+            {icon}
+            <Meta title={title} description={subtitle} />
+        </Card>
+    );
+
 }
