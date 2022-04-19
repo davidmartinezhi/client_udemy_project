4c2818d995ece37117ed38c3e52dff2f21989787
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-18 21:38:58 -0500

    Agregado estilo al footer

--- src/components/Web/Footer/Footer.js
+++ src/components/Web/Footer/Footer.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import {Layout, Row, Col} from 'antd'
 
 import './Footer.scss'
 
 export default function Footer() {
+    const {Footer} = Layout;
   return (
-    <div><h1>Footer...</h1></div>
+    <Footer className='footer'>
+        <Row>
+            <Col md={4}></Col>
+            <Col md={16}>
+                <Row>
+                    <Col md={8}>
+                        Mi información...
+                    </Col>
+                    <Col md={8}>
+                        Navegación...
+                    </Col>
+                    <Col md={8}>
+                        Newsletter...
+                    </Col>
+                </Row>
+                <Row className='footer__copyright'>
+                    <Col md={12}>
+                        © 2019 ALL RIGHTS RESERVED
+                    </Col>
+                    <Col md={12}>
+                        David Gerardo Martínez Hidrogo
+                    </Col>
+                </Row>
+            </Col>
+            <Col md={4}></Col>
+        </Row>
+    </Footer>
   )
 }
