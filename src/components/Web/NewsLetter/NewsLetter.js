6cfe38c1471321b2579ca5199a4903c497736995
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-22 16:29:56 -0500

    Formulario creado

--- src/components/Web/NewsLetter/NewsLetter.js
+++ src/components/Web/NewsLetter/NewsLetter.js
@@ -1,11 +1,34 @@
 import React from 'react'
-
+import { Form, Input, Button, notification } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
 import './NewsLetter.scss'
 
+
 export default function NewsLetter() {
+
+    const onSubmit = () => {
+        console.log("Newsletter Enviado...");
+    }
+
   return (
-    <div>
-        <NewsLetter />
+    <div className='newsletter'>
+        <h3>Newsletter</h3>
+
+        <Form onFinish={onSubmit} >
+            <Form.Item>
+                <Input 
+                    prefix={<UserOutlined style={{color: "rgba(0,0,0,0.25)"}} />}
+                    placeholder="Correo electronico"
+                    //value=""
+                    //onChange={}
+                />
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-form">
+                    ¡Me suscribo!
+                </Button>
+            </Form.Item>
+        </Form>
     </div>
   );
 }
