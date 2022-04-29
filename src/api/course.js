7c084e0134348632197815f50278fd5d61c7eff1
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-29 13:00:28 -0500

    Creado api para obtener info de cursos en udemy

--- src/api/course.js
+++ src/api/course.js
@@ -14,3 +14,23 @@ export function getCoursesApi() {
             return err;
         })
 }
+
+export function getCourseDataUdemyApi(id) {
+    //Obtención de datos de un curso de udemy
+
+    const baseUrl = `https://www.udemy.com/api-2.0/courses/${id}`;
+    const coursesParams = `?fields[course]=title,headline,url,price,image_480x270`;
+
+    const url = baseUrl + coursesParams;
+
+    return fetch(url)
+        .then( async response => {
+            return {code: response.status, data: await response.json()}
+        })
+        .then( result => {
+            return result;
+        })
+        .catch( error => {
+            return error;
+        });
+}
