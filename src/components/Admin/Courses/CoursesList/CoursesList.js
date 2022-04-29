428600883036e0f1f56d1cbfd157d68b11b0e10f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-29 13:20:41 -0500

    Creado botno para crear curso

--- src/components/Admin/Courses/CoursesList/CoursesList.js
+++ src/components/Admin/Courses/CoursesList/CoursesList.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { List, Button, Modal as ModalAntd, notification } from 'antd'
+import {} from '@ant-design/icons'
+import DragSortableList from "react-drag-sortable"
+import Modal from '../../../Modal'
+
+import { getCourseDataUdemyApi } from '../../../../api/course'
 import './CoursesList.scss'
+
+const { confirm } = ModalAntd;
+
 export default function CoursesList( props ) {
     const { courses, setReloadCourses } = props;
-    console.log(props);
+    const [listCourses, setListCourses] = useState([]);
+    const [isVisibleModal, setIsVisibleModal] = useState(false);
+    const [modalTitle, setModalTitle] = useState("");
+    const [modalContent, setModalContent] = useState(null);
+
+
   return (
-    <div>CoursesList...</div>
+    <div className='courses-list'>
+        <div className='course-list__header'>
+            <Button type="primary" onClick={() => console.log("Creando curso...")}>
+                Nuevo Curso
+            </Button>
+
+        </div>
+    </div>
   )
 }
