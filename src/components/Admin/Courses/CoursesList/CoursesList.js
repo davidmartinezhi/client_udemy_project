b38fbf2147741b6f5775e83cb83bd2edf389ca54
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-04-29 12:43:19 -0500

    Importado coursesList

---
+++ src/components/Admin/Courses/CoursesList/CoursesList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './CoursesList.scss'
+export default function CoursesList( props ) {
+    const { courses, setReloadCourses } = props;
+    console.log(props);
+  return (
+    <div>CoursesList...</div>
+  )
+}
