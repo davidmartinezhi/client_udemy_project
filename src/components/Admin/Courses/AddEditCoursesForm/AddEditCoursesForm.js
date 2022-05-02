9050026f894580a4421f03517588fbf2fd945318
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-02 12:48:21 -0500

    Creada carpeta para crear o editar cursos

---
+++ src/components/Admin/Courses/AddEditCoursesForm/AddEditCoursesForm.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react';
+
+import './AddEditCoursesForm.scss';
+
+export default function AddEditCoursesForm() {
+  return (
+    <div>AddEditCoursesForm</div>
+  )
+}
