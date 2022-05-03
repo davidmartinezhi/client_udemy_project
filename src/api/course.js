6cf530d91a03c7b1d47567b28e65fb5e505138c2
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-03 15:44:04 -0500

    cración de cursos exitoso

--- src/api/course.js
+++ src/api/course.js
@@ -57,3 +57,27 @@ export function deleteCourseApi(token, id) {
       return err;
     });
 }
+
+export function addCourseApi(token, course) {
+  const url = `${basePath}/${apiVersion}/add-course`;
+
+  const params = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify(course),
+  };
+
+  return fetch(url, params)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      return result;
+    })
+    .catch((err) => {
+      return err;
+    });
+}
--- src/components/Admin/Courses/AddEditCoursesForm/AddEditCoursesForm.js
+++ src/components/Admin/Courses/AddEditCoursesForm/AddEditCoursesForm.js
@@ -4,53 +4,95 @@ import {
   KeyOutlined,
   DollarCircleOutlined,
   GiftOutlined,
+  LinkOutlined
 } from "@ant-design/icons";
 import { getAccessTokenApi } from "../../../../api/auth";
+import { addCourseApi } from "../../../../api/course";
 
 import "./AddEditCoursesForm.scss";
 
 export default function AddEditCoursesForm(props) {
   const { setIsVisibleModal, setReloadCourses, course } = props;
-
   const [courseData, setCourseData] = useState({});
+
+  const addCourse = e => {
+    
+    if(!courseData.idCourse){
+        notification["error"]({message: "El id del curso es obligatorio"});
+    }else{
+        const accessToken = getAccessTokenApi();
+
+        addCourseApi(accessToken, courseData)
+            .then( response => {
+                const typeNotification = response.code == 200 ? "success" : "warning";
+                notification[typeNotification]({message: response.message});
+                setIsVisibleModal(false);
+                setReloadCourses(true);
+                setCourseData({});
+            })
+            .catch( () => {
+                notification["error"]({message: "Error del servidor, intentelo más tarde."});
+            })
+    }
+  }
+
+  const updateCourse = e => {
+      console.log("Actualizando curso...");
+  }
 
   return (
     <div className="add-edit-course-form">
-      <AddEditForm course={course} />
+      <AddEditForm
+        course={course}
+        addCourse={addCourse}
+        updateCourse={updateCourse}
+        courseData={courseData}
+        setCourseData={setCourseData}
+      />
     </div>
   );
 }
 
 function AddEditForm(props) {
-  const { course } = props;
+  const { course, addCourse, updateCourse, courseData, setCourseData } = props;
+
+
   return (
     <Form
       className="form-add-edit"
-      onFinish={() => console.log("Submit form...")}
+      onFinish={course ? updateCourse : addCourse}
     >
       <Form.Item>
         <Input
           prefix={<KeyOutlined />}
           placeholder="ID del curso"
-          // value={}
-          // onChange={}
+          value={courseData.idCourse}
+          onChange={e => setCourseData({...courseData, idCourse: e.target.value})}
           disabled={course ? true : false}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Input
+          prefix={<LinkOutlined />}
+          placeholder="URL del curso"
+          value={courseData.link}
+          onChange={e => setCourseData({...courseData, link: e.target.value})}
         />
       </Form.Item>
       <Form.Item>
         <Input
           prefix={<GiftOutlined />}
           placeholder="Cupón de descuento"
-          // value={}
-          // onChange={}
+          value={courseData.coupon}
+          onChange={e => setCourseData({...courseData, coupon: e.target.value})}
         />
       </Form.Item>
       <Form.Item>
         <Input
           prefix={<DollarCircleOutlined />}
           placeholder="Precio del curso"
-          // value={}
-          // onChange={}
+          value={courseData.price}
+          onChange={e => setCourseData({...courseData, price: e.target.value})}
         />
       </Form.Item>
       <Form.Item>
