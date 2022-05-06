bf56a87532b0b32db82b422762663ed0683769a8
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-06 11:51:55 -0500

    Info pasada correctamente a componente Course

--- src/components/Web/Courses/CoursesList/CoursesList.js
+++ src/components/Web/Courses/CoursesList/CoursesList.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {Row, Col, Button, notification, Card, Rate} from "antd";
+import {getCoursesApi} from "../../../../api/course";
 import "./CoursesList.scss";
 
-export default function CoursesList() {
+export default function CoursesList(props) {
+
+  const {courses} = props;
+  //console.log(courses);
   return (
-    <div>CoursesList...</div>
+    <div className="courses-list">
+      <Row>
+        {
+          courses.map( course => (
+            <Col md={8} className="courses-list__courset">
+              <Course key={course._id} course={course} />
+            </Col>
+          ))
+        }
+      </Row>
+    </div>
   )
+}
+
+
+function Course(props){
+
+  const { course } = props;
+
+  console.log(course);
+
+  return <p>hola...</p>
 }
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,17 +1,40 @@
 import React, {useState, useEffect} from 'react';
-import {Row, Col, notification, spin} from 'antd';
+import {Row, Col, notification, Spin} from 'antd';
 import { getCoursesApi } from "../api/course";
 import CoursesList from '../components/Web/Courses/CoursesList';
 import PresentationCourses from '../components/Web/Courses/PresentationCourses/PresentationCourses';
 
 export default function Courses() {
+  const [courses, setCourses] = useState(null);
+  // console.log(courses);
+  useEffect(() => {
+    getCoursesApi()
+      .then((response) => {
+        if(response?.code !== 200){
+          notification["warning"]({message: response.message});
+        }
+        else{
+          setCourses(response.courses);
+        }
+      })
+      .catch(() => {
+        notification["error"]({message: "Error del servidor, intentelo más tarde."});
+      }) 
+  }, [])
+  
+
   return (
     <>
       <Row>
         <Col md={4}></Col>
         <Col md={16}>
           <PresentationCourses />
-          <CoursesList />
+          {!courses ? (
+            <Spin tipe="Cargando cursos" style={{textAlign: "center", width: "100%", padding: "20px"}} />
+
+          ) : (
+            <CoursesList courses={courses} />
+          )}
         </Col>
         <Col md={4}></Col>
       </Row>
