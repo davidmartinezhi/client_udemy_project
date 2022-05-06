bbcac4c0c8f3b874fc754dcf5ed6522dd92f7fa9
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-06 12:11:33 -0500

    Creada tarjeta para desplegar info de los cursos

--- src/components/Web/Courses/CoursesList/CoursesList.js
+++ src/components/Web/Courses/CoursesList/CoursesList.js
@@ -1,33 +1,61 @@
-import React, {useState, useEffect} from "react";
-import {Row, Col, Button, notification, Card, Rate} from "antd";
-import {getCoursesApi} from "../../../../api/course";
+import React, { useState, useEffect } from "react";
+import { Row, Col, Button, notification, Card, Rate } from "antd";
+import { getCourseDataUdemyApi } from "../../../../api/course";
 import "./CoursesList.scss";
 
 export default function CoursesList(props) {
-
-  const {courses} = props;
+  const { courses } = props;
   //console.log(courses);
   return (
     <div className="courses-list">
       <Row>
-        {
-          courses.map( course => (
-            <Col md={8} className="courses-list__courset">
-              <Course key={course._id} course={course} />
-            </Col>
-          ))
-        }
+        {courses.map((course) => (
+          <Col md={8} className="courses-list__courset">
+            <Course key={course._id} course={course} />
+          </Col>
+        ))}
       </Row>
     </div>
-  )
+  );
 }
 
-
-function Course(props){
-
+function Course(props) {
   const { course } = props;
+  const {Meta} = Card;
+  const [courseInfo, setCourseInfo] = useState({});
+  
 
-  console.log(course);
+  useEffect(() => {
+    //Recibimos data por parte de udemy
+    getCourseDataUdemyApi(course.idCourse)
+      .then((response) => {
+        //Checa que la respuesta sea correcta
+        if (response?.code !== 200) {
+          //Si no es correcta la data, manda un aviso
+          notification["warning"]({ message: response.message });
+        } else {
+          //Si es correcta, establece los datos de la tarjeta
+          setCourseInfo(response.data);
+        }
+      })
+      .catch(() => {
+        //Si llega algún error del servidor, lo notificamos
+        notification["error"]({
+          message: "Error del servidor, intentelo más tarde.",
+        });
+      });
+  }, [course]);
 
-  return <p>hola...</p>
+  return (
+    <a href="#" target="_blank" rel="noopener noreferrer">
+      <Card
+        cover={<img src={courseInfo.image_480x270} alt={courseInfo.title} />}
+      >
+        <Meta title={courseInfo.title} description={courseInfo.headline} />
+        <div className="courses-list__course-footer">
+          <span>{course.price ? `MX$${course.price}` : courseInfo.price} </span>
+        </div>
+      </Card>
+    </a>
+  );
 }
