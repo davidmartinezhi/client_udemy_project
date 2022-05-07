62552f1fcb7dab666546cb9b7820339708d82374
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-07 16:58:08 -0500

    Borra info de un curso del form

--- src/components/Admin/Courses/AddEditCoursesForm/AddEditCoursesForm.js
+++ src/components/Admin/Courses/AddEditCoursesForm/AddEditCoursesForm.js
@@ -16,7 +16,7 @@ export default function AddEditCoursesForm(props) {
   const [courseData, setCourseData] = useState({});
 
   useEffect(() => {
-    course && setCourseData(course);
+    course ? setCourseData(course) : setCourseData({});
   }, [course]);
 
   const addCourse = (e) => {
