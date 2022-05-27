43a3dec3d6c9b89e3268e03d4a021a01298fb7bc
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-27 18:25:54 -0500

    initial setup tiny

--- src/components/Admin/Blog/AddEditPostForm/AddEditPostForm.js
+++ src/components/Admin/Blog/AddEditPostForm/AddEditPostForm.js
@@ -1,68 +1,100 @@
-import React, {useState, useEffect} from 'react'
-import {Row, Col, Form, Input, Button, DatePicker, notification} from 'antd'
-import {FontSizeOutlined, LinkOutlined} from '@ant-design/icons'
-import moment from 'moment'
-import { Editor } from '@tinymce/tinymce-react'
-import {getAccessTokenApi} from '../../../../api/auth'
+import React, { useState, useEffect } from "react";
+import { Row, Col, Form, Input, Button, DatePicker, notification } from "antd";
+import { FontSizeOutlined, LinkOutlined } from "@ant-design/icons";
+import moment from "moment";
+import { Editor } from "@tinymce/tinymce-react";
+import { getAccessTokenApi } from "../../../../api/auth";
 
-import './AddEditPostForm.scss'
+import "./AddEditPostForm.scss";
 
 export default function AddEditPostForm(props) {
-    const {setIsVisibleModal, setReloadPosts, post} = props;
-    const [postData, setPostData] = useState({});
+  const { setIsVisibleModal, setReloadPosts, post } = props;
+  const [postData, setPostData] = useState({});
 
-    useEffect(() => {
-      if(post){
-          setPostData(post);
-      }else{
-          setPostData({});
-      }
-    }, []);
-    
+  useEffect(() => {
+    if (post) {
+      setPostData(post);
+    } else {
+      setPostData({});
+    }
+  }, []);
 
   return (
-    <div className='add-edit-post-form'>
-        <AddEditForm postData={postData} setPostData={setPostData} post={post}/>
+    <div className="add-edit-post-form">
+      <AddEditForm postData={postData} setPostData={setPostData} post={post} />
     </div>
-  )
+  );
 }
 
-function AddEditForm(props){
-    const {postData, setPostData, post} = props;
+function AddEditForm(props) {
+  const { postData, setPostData, post } = props;
 
-    return(
-        <Form
-            className='add-edit-form'
-            layout="inline"
-        >
-            <Row gutter={24}>
-                <Col span={8}>
-                <Input 
-                    prefix={<FontSizeOutlined />}
-                    placeholder="Titulo"
-                    //value={}
-                    //onChange={}
-                />
-                </Col>
-                <Col span={8}>
-                <Input 
-                    prefix={<LinkOutlined />}
-                    placeholder="URL"
-                    //value={}
-                    //onChange={}
-                />
-                </Col>
-                <Col span={8}>
-                <DatePicker 
-                    style={{width: "100%"}}
-                    format="DD/MM/YYYY HH:mm:ss"
-                    placeholder="Fecha de publicación"
-                    showTime={{defaultValue: moment("00:00:00", "HH:mm:ss")}}
-                    //value={}
-                    //onChange={}
-                />
-                </Col>
-            </Row>
-        </Form>
-    )
+  return (
+    <Form className="add-edit-form" layout="inline">
+      <Row gutter={24}>
+        <Col span={8}>
+          <Input
+            prefix={<FontSizeOutlined />}
+            placeholder="Titulo"
+            //value={}
+            //onChange={}
+          />
+        </Col>
+        <Col span={8}>
+          <Input
+            prefix={<LinkOutlined />}
+            placeholder="URL"
+            //value={}
+            //onChange={}
+          />
+        </Col>
+        <Col span={8}>
+          <DatePicker
+            d
+            format="DD/MM/YYYY HH:mm:ss"
+            placeholder="Fecha de publicación"
+            showTime={{ defaultValue: moment("00:00:00", "HH:mm:ss") }}
+            //value={}
+            //onChange={}
+          />
+        </Col>
+      </Row>
+
+      <Editor
+        apiKey="your-api-key"
+        value=""
+        init={{
+          height: 400,
+          menubar: true,
+          plugins: [
+            "advlist",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "code",
+            "fullscreen",
+            "insertdatetime",
+            "media",
+            "table",
+            "code",
+            "help",
+            "wordcount",
+          ],
+          toolbar:
+            "undo redo | blocks | " +
+            "bold italic forecolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+      />
+    </Form>
+  );
 }
