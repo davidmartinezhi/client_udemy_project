b893598fc00e46cacfde634c9c824a4693b1e657
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-05-31 13:46:20 -0500

    creada estructura para saber si estamos en blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Row, Col } from "antd";
+import { withRouter } from "react-router-dom";
 
-export default function Blog() {
+ function Blog(props) {
+    const {location} = props;
+    const pathname = location.pathname;
+
+    console.log(pathname);
+
   return (
-    <div><h1>Blog</h1></div>
-  )
+    <div>
+      {pathname !== "/blog" ? <h1>Dentro de un post...</h1> : <h1>Lista de posts</h1>}
+    </div>
+  );
 }
+
+export default withRouter(Blog);
