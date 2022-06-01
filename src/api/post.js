36376b7d6969fe15c8045e81cf111fa710a8fa59
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-06-01 17:11:22 -0500

    creada api para obtener info del post

--- src/api/post.js
+++ src/api/post.js
@@ -85,3 +85,18 @@ export function updatePostApi(token, id, data) {
       return err;
     });
 }
+
+export function getPostApi(urlPost){
+  const url = `${basePath}/${apiVersion}/get-post/${urlPost}`;
+
+  return fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      return result;
+    })
+    .catch( err => {
+      return err;
+    })
+}
