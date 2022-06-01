6578c222692fb94804d4442ab2cd58de9a08c179
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-06-01 16:49:57 -0500

    despliegue de posts exitoso

--- src/components/Web/Blog/PostListWeb/PostListWeb.js
+++ src/components/Web/Blog/PostListWeb/PostListWeb.js
@@ -15,42 +15,53 @@ export default function PostListWeb(props) {
   const { page = 1 } = queryString.parse(location.search);
 
   useEffect(() => {
-      getPostsApi(12, page)
-      .then(response => {
-          if(response?.code !== 200){
-            notification["warning"]({message: response.message})
-          }
-          else{
-              setPosts(response.posts);
-          }
+    getPostsApi(12, page)
+      .then((response) => {
+        if (response?.code !== 200) {
+          notification["warning"]({ message: response.message });
+        } else {
+          setPosts(response.posts);
+        }
       })
-      .catch(() =>{
-          notification["error"]({message: "Error del servidor."})
-      })
+      .catch(() => {
+        notification["error"]({ message: "Error del servidor." });
+      });
   }, [page]);
 
-  if(!posts){
+  if (!posts) {
     return (
-      <Spin tip="cargando" style={{width: "100%", padding: "200px 0"}} />
+      <Spin tip="cargando" style={{ width: "100%", padding: "200px 0" }} />
     );
   }
 
   return (
     <div className="posts-list-web">
-    <h1>Blog</h1>
-    <List
-      dataSource={posts.docs}
-      renderItem={post => <Post post={post} />}
-    />
-    <Pagination posts={posts} location={location} history={history} />
-  </div>
+      <h1>Blog</h1>
+      <List
+        dataSource={posts.docs}
+        renderItem={(post) => <Post post={post} />}
+      />
+      <Pagination posts={posts} location={location} history={history} />
+    </div>
   );
 }
 
-function Post(props){
+function Post(props) {
+  const { post } = props;
+  console.log(post);
+  const day = moment(post.date).format("DD");
+  const month = moment(post.date).format("MMMM");
 
   return (
-    <h1>Post</h1>
-  );
+    <List.Item className="post">
+            <Link to={`blog/${post.url}`}>
+        <List.Item.Meta title={post.title} />
+      </Link>
+      <div className="post__date">
+        <span>{day}</span>
+        <span>{month}</span>
+      </div>
 
+    </List.Item>
+  );
 }
