d4a0506f695b0fd0e0ce8cdfc62dd0bcb3c445e8
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-06-04 14:51:12 -0500

    hashbrower

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import routes from './config/routes';
 import AuthProvider from './providers/AuthProvider'; 
 
